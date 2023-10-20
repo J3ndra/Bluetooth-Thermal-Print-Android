0b6b1980e31a78058fca89410d04c507f4ba2edf
Juianto <endra.kartika.25@gmail.com>
2023-10-20 11:42:45 +0700

    🚩 feat: Add isSdkInitialized in Sale Fragment

--- app/src/main/java/com/junianto/edcsekolah/menu/sale/SaleFragment.kt
+++ app/src/main/java/com/junianto/edcsekolah/menu/sale/SaleFragment.kt
@@ -1,21 +1,31 @@
 package com.junianto.edcsekolah.menu.sale
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.junianto.edcsekolah.AppViewModel
 import com.junianto.edcsekolah.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SaleFragment : Fragment() {
+
+    private val appViewModel: AppViewModel by viewModels()
 
     private lateinit var cvCash: CardView
     private lateinit var cvInsertCard: CardView
     private lateinit var cvMagneticCard: CardView
+
+    private var isSdkInitialized = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +37,10 @@ class SaleFragment : Fragment() {
         cvCash = rootView.findViewById(R.id.cv_cash)
         cvInsertCard = rootView.findViewById(R.id.cv_insert_card)
         cvMagneticCard = rootView.findViewById(R.id.cv_magnetic_card)
+
+        appViewModel.appSetup.observe(viewLifecycleOwner) {
+            isSdkInitialized = it.is_sdk_initialized
+        }
 
         return rootView
     }
@@ -41,15 +55,23 @@ class SaleFragment : Fragment() {
         }
 
         cvInsertCard.setOnClickListener {
-            findNavController().navigate(R.id.action_saleFragment_to_cashEnterAmountFragment, Bundle().apply {
-                putString("paymentType", "ICCARD")
-            })
+            if (isSdkInitialized) {
+                findNavController().navigate(R.id.action_saleFragment_to_cashEnterAmountFragment, Bundle().apply {
+                    putString("paymentType", "ICCARD")
+                })
+            } else {
+                Toast.makeText(requireContext(), R.string.perangkat_tidak_didukung, Toast.LENGTH_SHORT).show()
+            }
         }
 
         cvMagneticCard.setOnClickListener {
-            findNavController().navigate(R.id.action_saleFragment_to_cashEnterAmountFragment, Bundle().apply {
-                putString("paymentType", "MAGCARD")
-            })
+            if (isSdkInitialized) {
+                findNavController().navigate(R.id.action_saleFragment_to_cashEnterAmountFragment, Bundle().apply {
+                    putString("paymentType", "MAGCARD")
+                })
+            } else {
+                Toast.makeText(requireContext(), R.string.perangkat_tidak_didukung, Toast.LENGTH_SHORT).show()
+            }
         }
 
 //        cvCashless.setOnClickListener {
